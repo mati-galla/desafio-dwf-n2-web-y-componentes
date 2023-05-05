d698aa2b8aaeabc96cbaf420cc2ac1cfdad62bcf
mati-galla <matias.jgallardo@gmail.com>
2023-05-04 22:58:03 -0300

    Sexto commit: htmls, scripts y css listos; desafio finalizado

--- scripts/componentes.js
+++ scripts/componentes.js
@@ -4,14 +4,16 @@ async function header(el) {
   const logo = await imagesData[indexLogo].url;
   el.innerHTML = `
 <div class="header__left">
-    <img class="logo" src="${logo}" alt="" />
+<a href=".">
+<img class="logo" src="${logo}" alt="" />
+</a>
 </div>
 
 <div class="header__right">
   <nav class="header__menu">
-    <a href="" class="menu-link">Portfolio</a>
-    <a href="" class="menu-link">Servicios</a>
-    <a href="" class="menu-link">Contacto</a>
+    <a href="./portfolio.html" class="menu-link">Portfolio</a>
+    <a href="./servicios.html" class="menu-link">Servicios</a>
+    <a href="./contacto.html" class="menu-link">Contacto</a>
   </nav>
 
   <div class="header__hamburger-menu">
@@ -26,10 +28,76 @@ async function mobileMenu(el) {
   el.innerHTML = `<div class="mobile-menu">
   <span class="mobile-menu__close-button">X</span>
   <nav class="mobile-menu__links">
-    <a href="" class="menu-link">Portfolio</a>
-    <a href="" class="menu-link">Servicios</a>
-    <a href="" class="menu-link">Contacto</a>
+    <a href="./portfolio.html" class="menu-link">Portfolio</a>
+    <a href="./servicios.html" class="menu-link">Servicios</a>
+    <a href="./contacto.html" class="menu-link">Contacto</a>
   </nav>
+</div>`;
+}
+
+async function card(cardType) {
+  const cardsList = [];
+  let cardTag;
+  let data;
+  let cardsNumber;
+  if (cardType == "services") {
+    data = await getServices();
+    cardTag = "div";
+    cardsNumber = data.length;
+  } else if (cardType == "repos") {
+    data = await getRepos();
+    cardTag = "a";
+    cardsNumber = data.length;
+  }
+
+  for (i = cardsNumber; i > 0; i--) {
+    cardsList.push(document.createElement(cardTag));
+    cardsList[cardsNumber - i].classList = "card";
+    if (cardType == "services") {
+      cardsList[cardsNumber - i].href = data[i - 1].url;
+    }
+    cardsList[cardsNumber - i].innerHTML = `<img src=${
+      data[i - 1].image
+    } alt="" class="card__image" />
+  <h2 class="card__title">${data[i - 1].title}</h2>
+  <p class="card__description">${data[i - 1].description}</p>`;
+  }
+
+  return cardsList;
+}
+
+async function contactForm(el) {
+  el.innerHTML = `<div class="contact__container">
+  <h2 class="contact__title">Escribime</h2>
+  <form class="contact-form">
+    <div class="contact-form__row">
+      <label class="contact-form__label" for="name">Nombre</label>
+      <input
+        class="contact-form__input"
+        type="text"
+        name="name"
+        placeholder="Tu nombre" />
+    </div>
+    <div class="contact-form__row">
+      <label class="contact-form__label" for="email">Email</label>
+      <input
+        class="contact-form__input"
+        type="text"
+        name="email"
+        placeholder="tu@mail.com" />
+    </div>
+    <div class="contact-form__row">
+      <label class="contact-form__label" for="message">Mensaje</label>
+      <textarea
+        name="message"
+        cols="30"
+        rows="10"
+        class="contact-form__textarea"></textarea>
+    </div>
+    <button class="contact-form__send-button">
+      Enviar <i class="fa-regular fa-paper-plane"></i>
+    </button>
+  </form>
 </div>`;
 }
 
@@ -51,73 +119,20 @@ async function footer(el) {
   const twitter = await imagesData[indexTwitter].url;
 
   el.innerHTML = ` <img class="logo" src="${logo}" alt="" />
-
-<div class="page-links">
-  <a href=""><i class="fas fa-house"></i> Home</a>
-  <a href=""><i class="fas fa-user"></i> Servicios</a>
-  <a href=""><i class="fas fa-phone"></i> Contacto</a>
-</div>
-
-<div class="social-links">
-  <a href=""><img src="${linkedin}" alt=""></a>
-  <a href=""><img src="${github}" alt=""></a>
-  <a href=""><img src="${twitter}" alt=""></a>
-</div>
-
-<div class="copyright-notice">
-  <span>&#169;2023 - https://apx.school</span>
-</div>`;
-}
-
-async function card(el, index, cardType) {
-  let cardEl;
-  let data;
-  if (cardType == "services") {
-    data = await getServices();
-    cardEl = document.createElement("div.");
-  } else if (cardType == "repos") {
-    data = await getRepos();
-    cardEl = document.createElement("a");
-    cardEl.href = data[index].url;
-  }
-  cardEl.classList = "card";
-  cardEl.innerHTML = `<img src=${data[index].image} alt="" class="card__image" />
-<h2 class="card__title">${data[index].title}</h2>
-<p class="card__description">${data[index].description}</p>`;
-  el.appendChild(cardEl);
-}
-
-async function contactForm(el) {
-  el.innerHTML = `<div class="contact__container">
-  <h2 class="contact__title">Escribime</h2>
-  <form class="contact-form" action="">
-    <div class="contact-form__row">
-      <label class="contact-form__label" for="fullname">Nombre</label>
-      <input
-        class="contact-form__input"
-        type="text"
-        id="fullname"
-        placeholder="Tu nombre" />
-    </div>
-    <div class="contact-form__row">
-      <label class="contact-form__label" for="email">Email</label>
-      <input
-        class="contact-form__input"
-        type="text"
-        id="email"
-        placeholder="tu@mail.com" />
-    </div>
-    <div class="contact-form__row">
-      <label class="contact-form__label" for="message">Mensaje</label>
-      <textarea
-        id="message"
-        cols="30"
-        rows="10"
-        class="contact-form__textarea"></textarea>
-    </div>
-    <button class="send-button">
-      Enviar <i class="fa-regular fa-paper-plane"></i>
-    </button>
-  </form>
-</div>`;
+  
+  <div class="page-links">
+    <a href="."><i class="fas fa-house"></i> Home</a>
+    <a href="./servicios.html"><i class="fas fa-user"></i> Servicios</a>
+    <a href="./contacto.html"><i class="fas fa-phone"></i> Contacto</a>
+  </div>
+  
+  <div class="social-links">
+    <a href="https://www.linkedin.com/"><img src="${linkedin}" alt=""></a>
+    <a href="https://github.com/mati-galla"><img src="${github}" alt=""></a>
+    <a href="https://twitter.com/MaatyGallardo"><img src="${twitter}" alt=""></a>
+  </div>
+  
+  <div class="copyright-notice">
+    <a href="https://apx.school">&#169;2023 - https://apx.school</a>
+  </div>`;
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,61 @@
+async function createServiceCards() {
+  const cardsContainerEl = document.querySelector(".cards-container");
+  const cardType = "services";
+  const cardsList = await card(cardType);
+  for (let i = 0; i < 3; i++) {
+    cardsContainerEl.appendChild(cardsList[i]);
+  }
+}
+
+async function createReposCards() {
+  const cardsContainerEl = document.querySelector(".cards-container");
+  const cardType = "repos";
+  const cardsList = await card(cardType);
+  const reposNumber = cardsList.length;
+  for (let i = 0; i < reposNumber; i++) {
+    cardsContainerEl.appendChild(cardsList[i]);
+  }
+  const repos = cardsContainerEl.querySelectorAll(".card");
+  for (let i = reposNumber; i > 3; i--) {
+    repos[i - 1].style.display = "none";
+  }
+  return reposNumber;
+}
+
+async function showHideCards(n) {
+  const repos = document.querySelectorAll(".card");
+  for (let i = n; i > 3; i--) {
+    if (repos[i - 1].style.display == "none") {
+      repos[i - 1].style.display = "initial";
+    } else if (repos[i - 1].style.display == "initial") {
+      repos[i - 1].style.display = "none";
+    }
+  }
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const values = Object.fromEntries(data.entries());
+  const msg = `Nombre: ${values.name}, Email: ${values.email}, Mensaje: ${values.message}`;
+  fetch("https://apx-api.vercel.app/api/utils/dwf", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ to: "matias.jgallardo@gmail.com", message: msg }),
+  });
+}
+
+async function addBackground() {
+  const imagesData = await getImages();
+
+  const indexBackground = await imagesData.findIndex(
+    (e) => e.title == "Background Doodle"
+  );
+  const backgroundImg = await imagesData[indexBackground].url;
+  const elsWithBg = document.querySelectorAll(".background");
+  elsWithBg.forEach((e) => (e.style.background = `url(${backgroundImg})`));
+}
+
 async function createHeader() {
   const headerEl = document.querySelector(".header");
   await header(headerEl);
@@ -19,17 +77,73 @@ async function createHeader() {
   });
 }
 
-async function createServiceCards() {
-  const cardsContainer = document.querySelector(".cards-container");
-  const cardType = "services";
-  for (let i = 2; i > -1; i--) {
-    await card(cardsContainer, i, cardType);
-  }
+async function createWelcome() {
+  const imagesData = await getImages();
+  const welcomeImage = document.querySelector(".welcome__image");
+  const welcomeImgIndex = await imagesData.findIndex(
+    (e) => e.title == "Rocket Icon"
+  );
+  welcomeImage.src = imagesData[welcomeImgIndex].url;
 }
 
-async function createContactForm() {
+async function createPresentation() {
+  const imagesData = await getImages();
+  const presentationPhoto = document.querySelector(".presentation__photo");
+  const presentationPhotoIndex = await imagesData.findIndex(
+    (e) => e.title == "Foto Propia"
+  );
+  presentationPhoto.src = imagesData[presentationPhotoIndex].url;
+}
+
+async function createServices() {
+  const imagesData = await getImages();
+  if (document.querySelector(".services__title-container")) {
+    const servicesImage = document.querySelector(".services__image");
+    const servicesImgIndex = await imagesData.findIndex(
+      (e) => e.title == "Icono Maletín"
+    );
+    servicesImage.src = imagesData[servicesImgIndex].url;
+  }
+  await createServiceCards();
+}
+
+async function createPortfolio() {
+  const imagesData = await getImages();
+  const portfolioImage = document.querySelector(".portfolio__image");
+  const portfolioImgIndex = await imagesData.findIndex(
+    (e) => e.title == "Icono Maletín"
+  );
+  portfolioImage.src = imagesData[portfolioImgIndex].url;
+  const reposNumber = await createReposCards();
+  const cardsContainerEl = document.querySelector(".cards-container");
+  const showHideButton = document.createElement("div");
+  showHideButton.classList = "portfolio__show-hide-button";
+  showHideButton.innerHTML = `<span>Ver mas</span> <span> 
+  <i class="fa-regular fa-square-caret-down"></i> </span>`;
+  showHideButton.addEventListener("click", function () {
+    const buttonText = showHideButton.querySelector("span");
+    if (buttonText.textContent == "Ver mas") {
+      buttonText.textContent = "Ver menos";
+      showHideButton.querySelector("i").classList =
+        "fa-regular fa-square-caret-up";
+    } else if (buttonText.textContent == "Ver menos") {
+      buttonText.textContent = "Ver mas";
+      showHideButton.querySelector("i").classList =
+        "fa-regular fa-square-caret-down";
+    }
+    showHideCards(reposNumber);
+  });
+  cardsContainerEl.appendChild(showHideButton);
+}
+
+async function createContact() {
   const contactEl = document.querySelector(".contact");
   await contactForm(contactEl);
+  const formEl = contactEl.querySelector(".contact-form");
+  formEl.addEventListener("submit", function (e) {
+    handleFormSubmit(e);
+    formEl.reset();
+  });
 }
 
 async function createFooter() {
@@ -37,32 +151,13 @@ async function createFooter() {
   await footer(footerEl);
 }
 
-async function addBackground() {
-  const imagesData = await getImages();
-  const indexBackground = await imagesData.findIndex(
-    (e) => e.title == "Background Doodle"
-  );
-  const backgroundImg = await imagesData[indexBackground].url;
-  const elsWithBg = document.querySelectorAll(".background");
-  elsWithBg.forEach((e) => (e.style.background = `url(${backgroundImg})`));
-}
-
 (async function () {
-  const imagesData = await getImages();
-  const welcomeImage = document.querySelector(".welcome__image");
-  const presentationPhoto = document.querySelector(".presentation__photo");
-  const welcomeImgIndex = await imagesData.findIndex(
-    (e) => e.title == "Rocket Icon"
-  );
-  const presentationPhotoIndex = await imagesData.findIndex(
-    (e) => e.title == "Foto Propia"
-  );
-  welcomeImage.src = imagesData[welcomeImgIndex].url;
-  presentationPhoto.src = imagesData[presentationPhotoIndex].url;
-
-  createHeader();
-  createServiceCards();
-  createContactForm();
-  createFooter();
-  addBackground();
+  if (document.querySelector(".header")) createHeader();
+  if (document.querySelector(".welcome")) createWelcome();
+  if (document.querySelector(".presentation")) createPresentation();
+  if (document.querySelector(".services")) createServices();
+  if (document.querySelector(".portfolio")) createPortfolio();
+  if (document.querySelector(".contact")) createContact();
+  if (document.querySelector(".footer")) createFooter();
+  if (document.querySelector(".background")) addBackground();
 })();
